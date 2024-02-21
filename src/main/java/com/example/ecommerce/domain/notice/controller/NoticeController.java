8580dcb2c89ed4cdb556178b3be9878391ac19c6
DooHwanKim92 <rmfpsvldj@gmail.com>
2024-02-21 23:48:27 +0900

    공지사항 수정/삭제 구현

--- src/main/java/com/example/ecommerce/domain/notice/controller/NoticeController.java
+++ src/main/java/com/example/ecommerce/domain/notice/controller/NoticeController.java
@@ -6,10 +6,12 @@ import com.example.ecommerce.domain.notice.entity.Notice;
 import com.example.ecommerce.domain.notice.service.NoticeService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.engine.jdbc.mutation.spi.BindingGroup;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -41,9 +43,52 @@ public class NoticeController {
         if(bindingResult.hasErrors()) {
             return "/notice/create";
         }
+        if(noticeCreateForm.getCategory().equals("유형선택")) {
+            bindingResult.rejectValue("category", "categoryInCorrect",
+                    "카테고리를 선택해주세요 😅");
+            return "/notice/create";
+        }
 
         this.noticeService.create(noticeCreateForm);
 
         return "redirect:/notice/list";
     }
+
+    @GetMapping("/detail/{id}")
+    public String noticeDetail(Model model, @PathVariable(value = "id") Long id) {
+        Notice notice = this.noticeService.findById(id);
+        model.addAttribute("notice",notice);
+        return "/notice/detail";
+    }
+
+    @GetMapping("/modify/{id}")
+    public String noticeModify(NoticeCreateForm noticeCreateForm, Model model, @PathVariable(value = "id") Long id) {
+        Notice notice = this.noticeService.findById(id);
+        model.addAttribute("notice",notice);
+        return "/notice/modify";
+    }
+
+    @PostMapping("/modify/{id}")
+    public String noticeModifyPost(@PathVariable(value = "id") Long id ,@Valid NoticeCreateForm noticeCreateForm, BindingResult bindingResult) {
+        if(noticeCreateForm.getCategory().equals("유형선택")) {
+            bindingResult.rejectValue("category", "categoryInCorrect",
+                    "카테고리를 선택해주세요 😅");
+            return "/notice/modify";
+        }
+        if(bindingResult.hasErrors()) {
+            return "/notice/modify";
+        }
+        Notice notice = this.noticeService.findById(id);
+        this.noticeService.modify(noticeCreateForm, notice);
+
+        return String.format("redirect:/notice/detail/%d",id);
+    }
+
+    @GetMapping("/remove/{id}")
+    public String noticeRemove(@PathVariable(value = "id") Long id) {
+        this.noticeService.remove(id);
+        return "redirect:/notice/list";
+    }
+
+
 }
--- src/main/java/com/example/ecommerce/domain/notice/service/NoticeService.java
+++ src/main/java/com/example/ecommerce/domain/notice/service/NoticeService.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -26,5 +27,29 @@ public class NoticeService {
                 .build();
 
         this.noticeRepository.save(notice);
+    }
+
+    public Notice findById(Long id) {
+        Optional<Notice> notice = this.noticeRepository.findById(id);
+        if (notice.isEmpty()) {
+            return null;
+        }
+        return notice.get();
+    }
+
+    public void modify(NoticeCreateForm noticeCreateForm, Notice notice) {
+        Notice modifyNotice = notice.toBuilder()
+                .category(noticeCreateForm.getCategory())
+                .title(noticeCreateForm.getTitle())
+                .content(noticeCreateForm.getContent())
+                .build();
+
+        this.noticeRepository.save(modifyNotice);
+    }
+
+    public void remove(Long id) {
+        Notice notice = findById(id);
+
+        this.noticeRepository.delete(notice);
     }
 }
